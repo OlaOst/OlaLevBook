73e994df08409ad998d4db5a659df4c7139da7a2
OlaOst <olaa81@gmail.com>
2015-08-13 21:58:17 +0200

    Add router

--- source/app.d
+++ source/app.d
@@ -2,10 +2,14 @@ import vibe.d;
 
 shared static this()
 {
+  auto router = new URLRouter;
+  router.get("/", &hello);
+  
 	auto settings = new HTTPServerSettings;
 	settings.port = 8080;
 	settings.bindAddresses = ["::1", "127.0.0.1"];
-	listenHTTP(settings, &hello);
+  
+	listenHTTP(settings, router);
 
 	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
 }
